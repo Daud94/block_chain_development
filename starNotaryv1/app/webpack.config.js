3440d4011d01252eaf93e8cbc2ef0d1e1a269fe2
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-11-21 14:52:42 +0100

    starNotaryv1

--- starNotaryv1/app/webpack.config.js
+++ starNotaryv1/app/webpack.config.js
@@ -2,14 +2,37 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: "./src/index.js",
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
-  ],
-  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{from: "./src/index.html", to: "index.html"}]
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
+        },
+        compress: true
+    },
+    resolve: {
+        fallback: {
+            "fs": false,
+            "tls": false,
+            "net": false,
+            "path": false,
+            "zlib": false,
+            "http": false,
+            "https": false,
+            "stream": false,
+            "crypto": false,
+            "os": false,
+            "assert": false,
+            "vm": false
+        }
+    }
 };
